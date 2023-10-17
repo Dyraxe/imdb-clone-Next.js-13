d32e308d8a0d66e5038be9617b95d999a9d0347d
Paulo Amorim <phdja25@gmail.com>
2023-10-17 07:46:11 -0300

    Add card component

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FiThumbsUp } from "react-icons/fi";
+function Card({ result }) {
+  return (
+    <div className="group cursor-pointer rounded-lg transition-shadow duration-200 sm:m-2 sm:border sm:border-slate-400 sm:p-3 sm:shadow-md sm:hover:shadow-slate-400">
+      <Link href={`movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original${
+            result.backdrop_path || result.poster_path
+          }`}
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+          alt={`${result.title || result.name}' poster`}
+          width={500}
+          height={300}
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+          className="transition-opacity duration-200 group-hover:opacity-80 sm:rounded-t-lg"
+        ></Image>
+        {/* Details section */}
+        <div className="p-2">
+          <p className="line-clamp-4 text-sm">{result.overview}</p>
+          <h2 className="truncate text-lg font-bold">
+            {result.title || result.name}
+          </h2>
+          <p className="flex items-center ">
+            {result.release_date || result.first_air_date}
+            <FiThumbsUp className="ml-3 mr-1 h-5" /> {result.vote_count}
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default Card;
--- src/app/components/Results.jsx
+++ src/app/components/Results.jsx
@@ -1,11 +1,11 @@
+import Card from "./Card";
+
 function Results({ results }) {
   //   if (results.length === 0) return null;
   return (
-    <div>
+    <div className="mx-auto max-w-6xl py-4 sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {results.map((result) => (
-        <div key={result.id}>
-          {result.title} | {result.original_title}
-        </div>
+        <Card key={result.id} result={result} />
       ))}
     </div>
   );
